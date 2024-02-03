545871290151b352d37672044657ae8c1f37cab9
JustWeiHao <e0960531@u.nus.edu>
2024-02-04 01:43:40 +0800

    Modify ExitCommandTest.java

--- src/test/java/raphael/command/ExitCommandTest.java
+++ src/test/java/raphael/command/ExitCommandTest.java
@@ -1,9 +1,14 @@
 package raphael.command;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ExitCommandTest {
 
     @Test
-    public void exitCommandTest() {
+    public void executeTest() {
         new ExitCommand().execute(null, null, null);
+    }
+    @Test
+    public void isExitTest() {
+        assertTrue(new ExitCommand().isExit());
     }
 }
